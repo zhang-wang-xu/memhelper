30ed4f75e87bd5769503a9c8dc631724eeed2ac8
ZhangZhilei <424465251@qq.com>
2020-05-25 17:59:44 +0800

    在PassageView中测试了StaticLayout的效果。

--- app/src/main/java/com/example/memhelper/view/PassageView.java
+++ app/src/main/java/com/example/memhelper/view/PassageView.java
@@ -5,6 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,33 +19,34 @@ import java.util.ArrayList;
 public class PassageView extends View {
     Paint paint;
     private ArrayList<PointF> graphics = new ArrayList<PointF>();
-    PointF point;
+    String text;
 
-    public PassageView(Context context) {
-        super(context);
-        paint = new Paint(); //设置一个笔刷大小是3的黄色的画笔
+    private void init(){
+        paint = new Paint();
         paint.setColor(Color.BLACK);
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setStrokeWidth(3);
+        paint.setTextSize(100);
+    }
+
+    public void setText(String t){
+        text = t;
+    }
+
+    public PassageView(Context context) {
+        super(context);
+        init();
     }
 
     public PassageView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        paint = new Paint(); //设置一个笔刷大小是3的黄色的画笔
-        paint.setColor(Color.RED);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setStrokeWidth(3);
+        init();
     }
 
     public PassageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        paint = new Paint(); //设置一个笔刷大小是3的黄色的画笔
-        paint.setColor(Color.BLUE);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setStrokeWidth(3);
+        init();
     }
 
 
@@ -54,11 +58,17 @@ public class PassageView extends View {
         invalidate(); //重新绘制区域
         return true;
     }
-    //在这里我们将测试canvas提供的绘制图形方法
+    //在这里测试canvas提供的绘制图形方法
     @Override
     protected void onDraw(Canvas canvas) {
-        for (PointF point : graphics) {
-            canvas.drawPoint(point.x, point.y, paint);
-        }
+        TextPaint textPaint = new TextPaint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize(50.0F);
+        StaticLayout layout = new StaticLayout(text, textPaint, getWidth() - 100, Layout.Alignment.ALIGN_NORMAL, 1.0F, 0.0F, true);
+        // 这里的参数300，表示字符串的长度，当满300时，就会换行，也可以使用“\r\n”来实现换行
+        canvas.save();
+        canvas.translate(100,100);//从100，100开始画
+        layout.draw(canvas);
+        canvas.restore();//别忘了restore
     }
 }
